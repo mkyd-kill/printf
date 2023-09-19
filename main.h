b7903804b9474a0e280204e7e841c09b8a47b7d3
Conny <conny.serite@yahoo.com>
2023-09-19 10:58:16 -0700

    updated header file

--- main.h
+++ main.h
@@ -1,5 +1,15 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+void print_buffer(char buffer[], int *buff_ind);
+int handle_print(const char *format, int *i, va_list args, char buffer[]);
+
 #endif
